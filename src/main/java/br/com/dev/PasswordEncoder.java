6fab78ed0b8ada606a642ab6e3e24eda42b94446
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2017-11-07 22:17:51 -0200

    Autorizacao seguranca

---
+++ src/main/java/br/com/dev/PasswordEncoder.java
@@ -0,0 +1,14 @@
+package br.com.dev;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * Created by FelipeWendt on 01/11/17.
+ */
+public class PasswordEncoder {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("devdojo"));
+    }
+}
+
